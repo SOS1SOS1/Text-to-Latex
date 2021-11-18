441b9b93942cd291ba2b58cdf2cef5e057fdc5a5
SOS1SOS1 <43181395+SOS1SOS1@users.noreply.github.com>
2021-11-18 13:10:47 -0500

    #1

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+var document = null;
+
 function uploadImage() {
     document.getElementById('fileid').click();
 }
 
 var loadFile = function(event) {
-	var image = document.getElementById('output');
-	image.src = URL.createObjectURL(event.target.files[0]);
+	// Display image
+	// var image = document.getElementById('output');
+	// image.src = URL.createObjectURL(event.target.files[0]);
+
+	// Getting file type
+	// event.target.files[0].type == "application/pdf"
+	// event.target.files[0].type == "image/jpeg"
 };
