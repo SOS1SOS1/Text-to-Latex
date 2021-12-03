aca35753a171a72c16607398d8974ac390b8b88e
SOS1SOS1 <43181395+SOS1SOS1@users.noreply.github.com>
2021-12-02 21:30:37 -0500

    Added drag and drop support and run model button

--- static/script.js
+++ static/script.js
@@ -1,31 +1,22 @@
 var file = null;
+var src = null;
 var image = document.getElementById('inputImage');
 var embed = document.getElementById('inputPDF');
 
-function uploadImage() {
-	document.getElementById('fileid').click();
-}
-
 function clearFile() {
-	if (file != null) {
-		// Clear last image/pdf display
-		image.src = "";
-		embed.src = "";
-		embed.style.height = "0%";
-	}
+	// Clear last image/pdf display
+	image.src = "";
+	embed.src = "";
+	embed.style.height = "0%";
+	document.getElementById('inputPDF').style.display = 'none';
+	document.getElementById('run_and_clear').style.display = 'none';
+	document.getElementById('upload').style.display = 'inline-block';
 }
 
-function clearAndDisable() {
+function loadFile(f) {
 	clearFile();
-	document.getElementById('fileid').value = '';
-	document.getElementById('clear').disabled = true;
-}
-
-var loadFile = async function(event) {
-	clearFile();
-	file = event.target.files[0];
-	console.log(file);
-	let src = URL.createObjectURL(file);
+	file = f;
+	src = URL.createObjectURL(file);
 	if (file.type.substring(0, 5) == "image") {
 		// Display image
 		image.src = src;
@@ -34,7 +25,13 @@ var loadFile = async function(event) {
 		embed.src = src;
 		embed.style.height = "90%";
 	}
-	
+
+	document.getElementById('inputPDF').style.display = 'inline-block';
+	document.getElementById('run_and_clear').style.display = 'inline-flex';
+	document.getElementById('upload').style.display = 'none';
+}
+
+var runModel = async function() {
 	// convert image src to base64data
 	const reader = new FileReader();
 	let blob = await fetch(src).then(r => r.blob());
@@ -44,17 +41,17 @@ var loadFile = async function(event) {
 		console.log(base64data);
 		makeRequest(base64data, file.type);
 	}
-	
-	document.getElementById('clear').disabled = false;
-	
-	// displays loading animation
+		
+	// displays loading animation and hides latex
 	document.getElementById('loading').classList.remove("hide");
+	document.getElementById('containsMath').classList.add("hide");
 	setTimeout(displayLatex, 2000);
 };
 
 function displayLatex() {
-	// hides loading animation
+	// hides loading animation and displays latex
 	document.getElementById('loading').classList.add("hide");
+	document.getElementById('containsMath').classList.remove("hide");
 }
 
 function makeRequest(base64data, filetype) {
@@ -64,8 +61,7 @@ function makeRequest(base64data, filetype) {
 		  'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({data: base64data, filetype: filetype}),
-	})
-	.then(function (response) {
+	}).then(function (response) {
 		return response.json();
 	}).then(function (textObj) {
 		// render new LaTeX
@@ -75,4 +71,44 @@ function makeRequest(base64data, filetype) {
 		MathJax.typesetPromise([node]).then(() => {
 		});
 	});
+}
+
+let dropArea = document.getElementById('drop-area');
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+	dropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults (e) {
+	e.preventDefault();
+	e.stopPropagation();
+}
+
+['dragenter', 'dragover'].forEach(eventName => {
+	dropArea.addEventListener(eventName, highlight, false);
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+	dropArea.addEventListener(eventName, unhighlight, false);
+});
+
+function highlight(e) {
+	dropArea.classList.add('highlight');
+}
+
+function unhighlight(e) {
+	dropArea.classList.remove('highlight');
+}
+
+dropArea.addEventListener('drop', handleDrop, false);
+
+function handleDrop(e) {
+	let dt = e.dataTransfer
+	let files = dt.files
+
+	handleFiles(files)
+}
+
+function handleFiles(files) {
+	([...files]).forEach(loadFile)
 }
